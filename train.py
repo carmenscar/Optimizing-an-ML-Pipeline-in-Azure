7fc32586610e343a4b5f395273d183276ac62d64
carmenscar <carmenscartezini@gmail.com>
2025-03-02 15:17:00 -0300

    adding code into train.py

--- train.py
+++ train.py
@@ -9,6 +9,8 @@ from sklearn.preprocessing import OneHotEncoder
 import pandas as pd
 from azureml.core.run import Run
 from azureml.data.dataset_factory import TabularDatasetFactory
+from azureml.core import Workspace, Dataset
+
 
 def clean_data(data):
     # Dict for cleaning data
@@ -54,15 +56,16 @@ def main():
     # TODO: Create TabularDataset using TabularDatasetFactory
     # Data is located at:
     # "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
-
-    ds = ### YOUR CODE HERE ###
+    url = "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"  # Substitua pela URL real
+    ds = TabularDataFactory.from_delimited_files(path=url)
+    #ds = ### YOUR CODE HERE ###
     
     x, y = clean_data(ds)
 
     # TODO: Split data into train and test sets.
 
-    ### YOUR CODE HERE ###a
-
+    ### YOUR CODE HERE ###
+    x_train, y_train, x_test, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
     model = LogisticRegression(C=args.C, max_iter=args.max_iter).fit(x_train, y_train)
 
     accuracy = model.score(x_test, y_test)
